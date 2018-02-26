c06163bb51ec93947edceb5a52a8833a1b9131d1
Sugamoto <merol-2015@5gym-chanion.net>
2018-02-26 18:45:22 +0200

    I fucking Retire.........In your Dreams

---
+++ Main.py
@@ -0,0 +1,25 @@
+import pygame
+
+class Vector:
+    x = 0
+    y = 0
+
+    
+class Entity:
+        pass
+
+
+
+
+
+Status = False
+
+while not Status:
+    for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                    Status = True
+                    
+        
+
+
+

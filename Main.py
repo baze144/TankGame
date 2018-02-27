294e8e45ddaced83e211f21fc771cfdc92a65a54
Sugamoto <merol-2015@5gym-chanion.net>
2018-02-27 14:52:21 +0200

    I created a Window class with 3 functions:1.Create a window 2.Re-title the window 3.Resize the window

--- Main.py
+++ Main.py
@@ -1,4 +1,22 @@
+#import pygame,sys
+#import os
 import pygame
+import sys
+import os
+
+class Window:
+    title = ""
+    def __init__(self,a,b):
+        pygame.init()
+    screen = pygame.display.set_mode((v.x,v.y))  
+    def Title():
+        input(title)
+        pygame.display.set_caption(title)
+    def setSize(x,y):
+        input(v.x)
+        input(v.y)
+        
+
 
 class Vector:
     x = 0
@@ -7,8 +25,6 @@ class Vector:
     
 class Entity:
         pass
-
-
 
 
 
